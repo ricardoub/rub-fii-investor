6a4659093ce9970b5b70d729ae16810bd4cf050c
Ricardo Bomfim <ricardoub@gmail.com>
2023-09-24 11:08:20 -0300

    SEEDER role, permission, user, fii

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -13,94 +13,192 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->rolePermissions();
-        $this->permissionPermissions();
-        $this->userPermissions();
+        $this->role();
+        $this->permission();
+        $this->user();
+
+        $this->fii();
     }
 
-    private function rolePermissions()
+    private function role()
     {
-        $list = Permission::create([
-            'name' => 'role-list',
-            'display_name' => 'LISTAR PERFIL',
-            'description' => 'Listar perfil',
-        ]);
-        $show = Permission::create([
-            'name' => 'role-show',
-            'display_name' => 'EXIBIR PERFIL',
-            'description' => 'Exibir perfil',
-        ]);
-        $create = Permission::create([
-            'name' => 'role-create',
-            'display_name' => 'INCLUIR PERFIL',
-            'description' => 'Incluir perfil',
-        ]);
-        $edit = Permission::create([
-            'name' => 'role-edit',
-            'display_name' => 'ALTERAR PERFIL',
-            'description' => 'Alterar perfil',
-        ]);
-        $delete = Permission::create([
-            'name' => 'role-delete',
-            'display_name' => 'EXCLUIR PERFIL',
-            'description' => 'excluir perfil',
-        ]);
+        $list = Permission::updateOrCreate(
+            ['name' => 'ROLE_LIST'],
+            [
+                'name' => 'ROLE_LIST',
+                'display_name' => 'LISTAR PERFIL',
+                'description' => 'Listar perfil',
+            ]
+        );
+        $show = Permission::updateOrCreate(
+            ['name' => 'ROLE_SHOW'],
+            [
+                'name' => 'ROLE_SHOW',
+                'display_name' => 'EXIBIR PERFIL',
+                'description' => 'Exibir perfil',
+            ]
+        );
+        $insert = Permission::updateOrCreate(
+            ['name' => 'ROLE_INSERT'],
+            [
+                'name' => 'ROLE_INSERT',
+                'display_name' => 'INCLUIR PERFIL',
+                'description' => 'Incluir perfil',
+            ]
+        );
+        $update = Permission::updateOrCreate(
+            ['name' => 'ROLE_UPDATE'],
+            [
+                'name' => 'ROLE_UPDATE',
+                'display_name' => 'ALTERAR PERFIL',
+                'description' => 'Alterar perfil',
+            ]
+        );
+        $delete = Permission::updateOrCreate(
+            ['name' => 'ROLE_DELETE'],
+            [
+                'name' => 'ROLE_DELETE',
+                'display_name' => 'EXCLUIR PERFIL',
+                'description' => 'Excluir perfil',
+            ]
+        );
     }
-    private function permissionPermissions()
+    private function permission()
     {
-        $list = Permission::create([
-            'name' => 'permission-list',
-            'display_name' => 'LISTAR PERMISSAO',
-            'description' => 'Listar permissões',
-        ]);
-        $show = Permission::create([
-            'name' => 'permission-show',
-            'display_name' => 'EXIBIR PERMISSAO',
-            'description' => 'Exibir permissão',
-        ]);
-        $create = Permission::create([
-            'name' => 'permission-create',
-            'display_name' => 'INCLUIR PERMISSAO',
-            'description' => 'Incluir permissão',
-        ]);
-        $edit = Permission::create([
-            'name' => 'permission-edit',
-            'display_name' => 'ALTERAR PERMISSAO',
-            'description' => 'Alterar permissão',
-        ]);
-        $delete = Permission::create([
-            'name' => 'permission-delete',
-            'display_name' => 'EXCLUIR PERMISSAO',
-            'description' => 'excluir permissão',
-        ]);
+        $list = Permission::updateOrCreate(
+            ['name' => 'PERMISSION_LIST'],
+            [
+                'name' => 'PERMISSION_LIST',
+                'display_name' => 'LISTAR PERMISSAO',
+                'description' => 'Listar permissão',
+            ]
+        );
+        $show = Permission::updateOrCreate(
+            ['name' => 'PERMISSION_SHOW'],
+            [
+                'name' => 'PERMISSION_SHOW',
+                'display_name' => 'EXIBIR PERMISSAO',
+                'description' => 'Exibir permissão',
+            ]
+        );
+        $insert = Permission::updateOrCreate(
+            ['name' => 'PERMISSION_INSERT'],
+            [
+                'name' => 'PERMISSION_INSERT',
+                'display_name' => 'INCLUIR PERMISSAO',
+                'description' => 'Incluir permissão',
+            ]
+        );
+        $update = Permission::updateOrCreate(
+            ['name' => 'PERMISSION_UPDATE'],
+            [
+                'name' => 'PERMISSION_UPDATE',
+                'display_name' => 'ALTERAR PERMISSAO',
+                'description' => 'Alterar permissão',
+            ]
+        );
+        $delete = Permission::updateOrCreate(
+            ['name' => 'PERMISSION_DELETE'],
+            [
+                'name' => 'PERMISSION_DELETE',
+                'display_name' => 'EXCLUIR PERMISSAO',
+                'description' => 'Excluir permissão',
+            ]
+        );
+    }
+    private function user()
+    {
+        $list = Permission::updateOrCreate(
+            ['name' => 'USER_LIST'],
+            [
+                'name' => 'USER_LIST',
+                'display_name' => 'LISTAR USUARIO',
+                'description' => 'Listar usuário',
+            ]
+        );
+        $show = Permission::updateOrCreate(
+            ['name' => 'USER_SHOW'],
+            [
+                'name' => 'USER_SHOW',
+                'display_name' => 'EXIBIR USUARIO',
+                'description' => 'Exibir usuário',
+            ]
+        );
+        $insert = Permission::updateOrCreate(
+            ['name' => 'USER_INSERT'],
+            [
+                'name' => 'USER_INSERT',
+                'display_name' => 'INCLUIR USUARIO',
+                'description' => 'Incluir usuário',
+            ]
+        );
+        $update = Permission::updateOrCreate(
+            ['name' => 'USER_UPDATE'],
+            [
+                'name' => 'USER_UPDATE',
+                'display_name' => 'ALTERAR USUARIO',
+                'description' => 'Alterar usuário',
+            ]
+        );
+        $delete = Permission::updateOrCreate(
+            ['name' => 'USER_DELETE'],
+            [
+                'name' => 'USER_DELETE',
+                'display_name' => 'EXCLUIR USUARIO',
+                'description' => 'Excluir usuário',
+            ]
+        );
+        $owner = Permission::updateOrCreate(
+            ['name' => 'USER_OWNER'],
+            [
+                'name' => 'USER_OWNER',
+                'display_name' => 'PROPRIO USUARIO',
+                'description' => 'Manter o próprio usuário',
+            ]
+        );
     }
 
-    private function userPermissions()
+    private function fii()
     {
-        $list = Permission::create([
-            'name' => 'user-list',
-            'display_name' => 'LISTAR USUARIO',
-            'description' => 'Listar os usuários cadastrados',
-        ]);
-        $show = Permission::create([
-            'name' => 'user-show',
-            'display_name' => 'EXIBIR USUARIO',
-            'description' => 'Exibir um usuário cadastrado',
-        ]);
-        $create = Permission::create([
-            'name' => 'user-create',
-            'display_name' => 'INCLUIR USUARIO',
-            'description' => 'Incluir um usuário no cadastro',
-        ]);
-        $edit = Permission::create([
-            'name' => 'user-edit',
-            'display_name' => 'ALTERAR USUARIO',
-            'description' => 'Alterar um usuário cadastrado',
-        ]);
-        $delete = Permission::create([
-            'name' => 'user-delete',
-            'display_name' => 'EXCLUIR USUARIO',
-            'description' => 'excluir um usuário cadastrado',
-        ]);
+        $list = Permission::updateOrCreate(
+            ['name' => 'FII_LIST'],
+            [
+                'name' => 'FII_LIST',
+                'display_name' => 'LISTAR FII',
+                'description' => 'Listar fundo imobiliário',
+            ]
+        );
+        $show = Permission::updateOrCreate(
+            ['name' => 'FII_SHOW'],
+            [
+                'name' => 'FII_SHOW',
+                'display_name' => 'EXIBIR FII',
+                'description' => 'Exibir fundo imobiliário',
+            ]
+        );
+        $insert = Permission::updateOrCreate(
+            ['name' => 'FII_INSERT'],
+            [
+                'name' => 'FII_INSERT',
+                'display_name' => 'INCLUIR FII',
+                'description' => 'Incluir fundo imobiliário',
+            ]
+        );
+        $update = Permission::updateOrCreate(
+            ['name' => 'FII_UPDATE'],
+            [
+                'name' => 'FII_UPDATE',
+                'display_name' => 'ALTERAR FII',
+                'description' => 'Alterar fundo imobiliário',
+            ]
+        );
+        $delete = Permission::updateOrCreate(
+            ['name' => 'FII_DELETE'],
+            [
+                'name' => 'FII_DELETE',
+                'display_name' => 'EXCLUIR FII',
+                'description' => 'Excluir fundo imobiliário',
+            ]
+        );
     }
 }
